adf52e47f02bf241d96d8c975f187f615a0f58b5
Rodrigo Pavez <rpavez@gmail.com>
2017-05-18 00:28:17 -0400

    updated files

--- src/base64.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-export function base64(i) {
-  return (new Buffer(i, 'ascii')).toString('base64');
-}
-
-export function unbase64(i) {
-  return (new Buffer(i, 'base64')).toString('ascii');
-}
